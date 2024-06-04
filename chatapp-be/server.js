01732da36832b2cabdcc1702937d06486179f9a4
waqarsaikhu <wsaikhou16@gmail.com>
2024-06-04 17:34:39 +0500

    1st part completed - backend

---
+++ chatapp-be/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import connectToMongoDB from "./db/connectToMongoDb.js";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+// app.get("/", (req, res) => {
+//   // root route handler http://localhost:5000
+//   res.send("Hello World");
+// });
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
+});
